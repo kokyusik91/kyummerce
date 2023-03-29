fb26e773194c8ce836d65d582cba4aa115089047
kks91 <kokyusik91@naver.com>
2023-03-29 23:44:11 +0900

    ✨ Notion API로 데이터 추가하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '../styles/Home.module.css'
 import Button from 'components/Button'
 import { useEffect, useRef, useState } from 'react'
+import { css } from '@emotion/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -52,8 +53,23 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <input type="text" ref={inputRef} />
-          <button onClick={handleClickFetch}>추가하기</button>
+          <input
+            className="placeholder:italic placeholder:text-slate-400 block bg-white w-100 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Search for anything..."
+            type="text"
+            name="search"
+            ref={inputRef}
+          />
+          <button
+            css={css`
+              background-color: hotpink;
+              padding: 16px;
+              border-radius: 8px;
+            `}
+            onClick={handleClickFetch}
+          >
+            추가하기
+          </button>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
