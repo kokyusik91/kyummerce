8922ae6c32eea7a4dd879d8cd137b75d0be4777d
kks91 <kokyusik91@naver.com>
2023-03-30 23:47:10 +0900

    ✨ implement useDebounce hook

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+const useDebounce = <T = any>(value: T, delay = 600) => {
+  const [debounceValue, setDebounceValue] = useState<T>(() => value)
+  console.log('실행')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+
+  return debounceValue
+}
+
+export default useDebounce
