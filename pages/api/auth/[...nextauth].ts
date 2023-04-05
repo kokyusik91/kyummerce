ed50767e2ee3797178807095e676a28af7271918
kks91 <kokyusik91@naver.com>
2023-04-05 14:11:43 +0900

    ✨ 쿠키 만료시간 측정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
@@ -6,7 +6,7 @@ import { CLIENT_ID, CLIENT_SECRET } from 'constants/google-oAuth'
 
 const prisma = new PrismaClient()
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -14,4 +14,10 @@ export default NextAuth({
       clientSecret: CLIENT_SECRET,
     }),
   ],
-})
+  session: {
+    strategy: 'database',
+    maxAge: 1 * 24 * 60 * 60,
+  },
+}
+
+export default NextAuth(authOptions)
